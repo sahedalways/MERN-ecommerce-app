a300b76e5051da4d7c25da23272a71e2c89328dc
Md. Sariful Islam <ssahed65@gmail.com>
2022-06-20 23:34:03 +0600

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,13 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  })
+}
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
